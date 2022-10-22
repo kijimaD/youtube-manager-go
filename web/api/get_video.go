1222d72725730ebbbaa8f3fd5b69f7cfb4af3574
kijimaD <norimaking777@gmail.com>
2022-10-22 22:16:09 +0900

    feat: add get video action

---
+++ web/api/get_video.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+	"github.com/valyala/fasthttp"
+	"google.golang.org/api/youtube/v3"
+)
+
+type VideoResponse struct {
+	VideoList *youtube.VideoListResponse `json: "video_list"`
+}
+
+func GetVideo() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		yts := c.Get("yts").(*youtube.Service) // youtube.Serviceはコンテキストから取得
+
+		videoId := c.Param("id") // 動画のidをパラメータから取得して、APIリクエスト時に使う
+
+		call := yts.Videos.
+			List([]string{"id", "snippet"}).
+			Id(videoId)
+
+		res, err := call.Do()
+		if err != nil {
+			logrus.Fatalf("Error calling YouTube API: %v", err)
+		}
+
+		// YouTube APIからのレスポンスをそのままnuxtに返却するのではなく、VideoResponseという名前の構造体に詰めて返却する
+		v := VideoResponse{
+			VideoList: res,
+		}
+
+		return c.JSON(fasthttp.StatusOK, v)
+	}
+}
