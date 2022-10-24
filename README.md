466d9b643e51bc843984447df336d7f4182b0cfd
kijimaD <norimaking777@gmail.com>
2022-10-24 20:11:04 +0900

    docs: write current problem

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 memo [NuxtとGoではじめるWebアプリ開発](https://tatsu-zine.com/books/nuxt-go-web-app-development)
 
+stopped at page 97, always return `Not Authenticated`.
+```
+ID token has invalid 'aud' (audience) claim; expected "xxx" but got "yyy"; make sure the ID token comes from the same Firebase project as the credential used to authenticate this SDK; see https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve a valid ID token
+```
+
 ## development
 ```shell
 # autoreload
--- middlewares/auth.go
+++ middlewares/auth.go
@@ -19,6 +19,8 @@ func verifyFirebaseIDToken(ctx echo.Context, auth *auth.Client) (*auth.Token, er
 }
 
 // ログインした状態でなければ利用できないAPIに対して使用する
+// FIXME: トークンの確認に失敗する。
+// ID token has invalid 'aud' (audience) claim; expected "nuxtgovideo" but got "manager-d606f"; make sure the ID token comes from the same Firebase project as the credential used to authenticate this SDK; see https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve a valid ID token
 func FirebaseGuard() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
